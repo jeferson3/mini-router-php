9ec960f859aff4a981e18f24d8786a2a9c69d340
jeferson <jefersongomes333@hotmail.com>
2021-04-15 23:15:28 -0300

    Update routes.php

--- routes.php
+++ routes.php
@@ -4,21 +4,9 @@ use App\Config\Router;
 
 Router::get("/", function (){
 
-    return "<a href='".Router::route('products.show',1)."'>Product 1</a>";
+    return "<a href='".Router::route('products.index')."'>Products</a>";
 });
 
-Router::get("/products", "ProductController@index")->name('products.index', );
-
-Router::get("/products/create", "ProductController@create");
-
-Router::post("/products/store","ProductController@store");
-
-Router::get("/products/{id}/show","ProductController@show")->name('products.show');
-
-Router::get("/products/{id}/edit","ProductController@edit");
-
-Router::post("/products/{id}/update","ProductController@update");
-
-Router::post("/products/{id}/delete","ProductController@delete");
+Router::resource('products', 'ProductController')->name('products');
 
 Router::init();
